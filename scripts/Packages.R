0f46254ee6637aa78dacce18746c4811d32cf902
Sierra Martin <sarebear323@gmail.com>
2023-07-15 23:15:22 -0700

    got scripts working, some graphs outputted, updated datasets

---
+++ scripts/Packages.R
@@ -0,0 +1,12 @@
+# Run this script for any packages that need to be installed for this project. 
+
+install.packages("bnlearn")
+install.packages("dplyr")
+#install.packages("dv cevtools") # not available in 4.3
+install.packages("devtools") # probably use this instead on 4.3
+#install_github("jtextor/dagitty/r") # can't find / use function
+if (!requireNamespace("BiocManager", quietly = TRUE))
+ install.packages("BiocManager")
+BiocManager::install("graph", version = "3.17")
+install.packages("psych")
+install.packages("lavaan")
--- semi_interleaved/semi_interleaved.R
+++ semi_interleaved/semi_interleaved.R
@@ -5,33 +5,49 @@ suppressWarnings({
     suppressPackageStartupMessages(library(dplyr))
     suppressPackageStartupMessages(library(devtools))
     suppressPackageStartupMessages(library(graph))
+    suppressPackageStartupMessages(library(psych))
+    suppressPackageStartupMessages(library(lavaan))
     })
 
+setwd("D:/source/repos/semi_interleaved")
+
+# run this to install required packages for this project.
+# source("scripts/Packages.R")
+
+# get Alwood data
+source("scripts/Alwood.R")
+
+# get Turner Histomorphometry data
+source("scripts/Turner_histo.R")
+
+# get Turner GLDS data
+source("scripts/Turner_GLDS.R")
+
+# get Ko data (only use DF named "ko")
+source("scripts/Ko.R")
+
 #-----------------------------------------------------------------
-#   Functions:
+#   Getting it into the algo
 #-----------------------------------------------------------------
 
-get_csv <- function(){
-    filepath <- file.choose()
-    if (!grepl("\\.csv$", filepath, ignore.case = TRUE)) {
-        message("ERROR: Please select a .csv file.")
-        return(get_csv())
-    }
-    return(filepath)
-}
-
-#-----------------------------------------------------------------
-#   Main:
-#-----------------------------------------------------------------
-path_osd_310 <- get_csv()
-data_osd_310 <- read.csv(path_osd_310, header = FALSE) # now in Data Frame
-# Headerless csv, just column names
-
-# remove first 3 columns and first row (Variable Names, use -c(0:1)) 
-# and preserve only numerical data
-sub_data_osd_310 <- data_osd_310[, -c(0:3)]
-sub_data_osd_310[] <- lapply(sub_data_osd_310, as.factor)
-
-si_structure <- si.hiton.pc(sub_data_osd_310, undirected = FALSE)
+# plot Alwood
+si_structure <- si.hiton.pc(alwood, undirected = FALSE)
 si_structure_g <- as.graphNEL(si_structure)
 plot(si_structure)
+
+# plot Turner Histomorphometry
+si_structure <- si.hiton.pc(turner, undirected = FALSE)
+si_structure_g <- as.graphNEL(si_structure)
+plot(si_structure)
+
+# plot Turner GLDS
+si_structure <- si.hiton.pc(glds, undirected = FALSE)
+si_structure_g <- as.graphNEL(si_structure)
+plot(si_structure)
+
+# plot Ko
+si_structure <- si.hiton.pc(ko, undirected = FALSE)
+si_structure_g <- as.graphNEL(si_structure)
+plot(si_structure)
+
+
